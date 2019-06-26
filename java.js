f5d7cc4cfd651af05ec58a945425de8a7e66b316
Nishant Singh <755083@student.tor.loc>
2019-06-26 14:16:43 -0400

    2 commit

---
+++ java.js
@@ -0,0 +1,13 @@
+function waitingFn(timeInMs) {
+
+    const futureTime = Date.now() + timeinms;
+    while (futureTime > Date.now()) {
+
+
+    }
+
+}
+
+waitingFn(3000);
+
+connsole.log("Funtion has endend")
